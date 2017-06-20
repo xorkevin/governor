7e285bb7644d976aff742773951f9d9398ac9cf5
Kevin Wang <wangkevin448@gmail.com>
2017-06-20 12:42:26 -0700

    redis cache

---
+++ service/cache/cache.go
@@ -0,0 +1,62 @@
+package cache
+
+import (
+	"github.com/go-redis/redis"
+	"github.com/hackform/governor"
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type (
+	// Cache is a service wrapper around a redis instance
+	Cache struct {
+		cache *redis.Client
+	}
+)
+
+const (
+	moduleID = "redis"
+)
+
+// New creates a new cache service
+func New(c governor.Config) (*Cache, error) {
+	v := c.Conf()
+	rconf := v.GetStringMapString("redis")
+
+	cache := redis.NewClient(&redis.Options{
+		Addr:     rconf["host"] + ":" + rconf["port"],
+		Password: rconf["password"],
+		DB:       v.GetInt("redis.dbname"),
+	})
+
+	if _, err := cache.Ping().Result(); err != nil {
+		return nil, err
+	}
+
+	return &Cache{
+		cache: cache,
+	}, nil
+}
+
+// Mount is a place to mount routes to satisfy the Service interface
+func (c *Cache) Mount(conf governor.Config, r *echo.Group, l *logrus.Logger) error {
+	return nil
+}
+
+const (
+	moduleIDHealth = moduleID + ".Health"
+)
+
+// Health is a health check for the service
+func (c *Cache) Health() *governor.Error {
+	if _, err := c.cache.Ping().Result(); err != nil {
+		return governor.NewError(moduleIDHealth, err.Error(), 0, http.StatusServiceUnavailable)
+	}
+	return nil
+}
+
+// Cache returns the sql database instance
+func (c *Cache) Cache() *redis.Client {
+	return c.cache
+}
---
+++ service/cache/conf/conf.go
@@ -0,0 +1,15 @@
+package cacheconf
+
+import (
+	"github.com/hackform/governor"
+)
+
+// Conf loads in the defaults for the database
+func Conf(c *governor.Config) error {
+	v := c.Conf()
+	v.SetDefault("redis.password", "admin")
+	v.SetDefault("redis.dbname", 0)
+	v.SetDefault("redis.host", "localhost")
+	v.SetDefault("redis.port", "6379")
+	return nil
+}
