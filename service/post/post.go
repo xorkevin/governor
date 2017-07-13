0e721a8d0638d6f86c5be884cda4cb906e21cf70
Kevin Wang <wangkevin448@gmail.com>
2017-07-13 01:46:19 -0700

    post service

--- service/post/post.go
+++ service/post/post.go
@@ -1,1 +1,78 @@
 package post
+
+import (
+	"github.com/hackform/governor"
+	"github.com/hackform/governor/service/cache"
+	"github.com/hackform/governor/service/db"
+	"github.com/hackform/governor/service/user/gate"
+	"github.com/labstack/echo"
+	"github.com/sirupsen/logrus"
+	"net/http"
+)
+
+type (
+	reqPostPost struct {
+		Postid  string `json:"postid"`
+		Userid  string `json:"userid"`
+		Tags    string `json:"group_tags"`
+		Content string `json:"content"`
+	}
+
+	resPost struct {
+		Postid       string `json:"postid"`
+		Userid       string `json:"userid"`
+		Tags         string `json:"group_tags"`
+		Content      string `json:"content"`
+		CreationTime int64  `json:"creation_time"`
+	}
+)
+
+func (r *reqPostPost) valid() *governor.Error {
+	return nil
+}
+
+const (
+	moduleID = "post"
+)
+
+type (
+	// Post is a service for creating posts
+	Post struct {
+		db    *db.Database
+		cache *cache.Cache
+		gate  *gate.Gate
+	}
+)
+
+// New creates a new Post service
+func New(conf governor.Config, l *logrus.Logger, db *db.Database, ch *cache.Cache) *Post {
+	ca := conf.Conf().GetStringMapString("userauth")
+
+	l.Info("initialized profile service")
+
+	return &Post{
+		db:    db,
+		cache: ch,
+		gate:  gate.New(ca["secret"], ca["issuer"]),
+	}
+}
+
+// Mount is a collection of routes for accessing and modifying post data
+func (p *Post) Mount(conf governor.Config, r *echo.Group, l *logrus.Logger) error {
+	r.POST("/", func(c echo.Context) error {
+		return c.NoContent(http.StatusNoContent)
+	}, p.gate.User())
+
+	r.GET("/:id", func(c echo.Context) error {
+		return c.JSON(http.StatusOK, &resPost{})
+	})
+
+	l.Info("mounted profile service")
+
+	return nil
+}
+
+// Health is a check for service health
+func (p *Post) Health() *governor.Error {
+	return nil
+}
