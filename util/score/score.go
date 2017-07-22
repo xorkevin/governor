9810ab7c69d54106ebab4210409e6f93560406a0
Kevin Wang <wangkevin448@gmail.com>
2017-07-22 14:30:12 -0700

    confidence score

--- util/score/score.go
+++ util/score/score.go
@@ -25,3 +25,29 @@ func Log(ups, downs int32, creationTime, epoch int64) int64 {
 	y := (creationTime - epoch) / timeSlope
 	return sign(x)*log(x) + y
 }
+
+const (
+	zscore       float64 = 1.64 // 90% confidence
+	zSqr         float64 = zscore * zscore
+	zSqrd2       float64 = zSqr / 2
+	zSqrd4       float64 = zSqr / 4
+	maxConfScore float64 = 8070450532247928832
+)
+
+// Confidence is the confidence sort
+func Confidence(ups, downs int32) int64 {
+	if ups == 0 {
+		return -int64(downs)
+	}
+
+	if ups+downs == 0 {
+		return 0
+	}
+	u := float64(ups)
+	d := float64(downs)
+	n := u + d
+
+	above := u + zSqrd2 - zscore*math.Sqrt(u*d/n+zSqrd4)
+	under := n + zSqr
+	return int64((above / under) * maxConfScore)
+}
