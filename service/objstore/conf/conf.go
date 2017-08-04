bbfb28a13662e275b1aad24a4ed4f35a46ee035b
Kevin Wang <wangkevin448@gmail.com>
2017-08-03 19:14:56 -0700

    setup object store

---
+++ service/objstore/conf/conf.go
@@ -0,0 +1,15 @@
+package objstoreconf
+
+import (
+	"github.com/hackform/governor"
+)
+
+// Conf loads in the defaults for the object store
+func Conf(c *governor.Config) error {
+	v := c.Conf()
+	v.SetDefault("minio.keyID", "")
+	v.SetDefault("minio.keySecret", "")
+	v.SetDefault("minio.host", "localhost:9000")
+	v.SetDefault("minio.sslmode", true)
+	return nil
+}
