263cc029497f5267d37a08e27e1f10ac6128d8b3
Kevin Wang <wangkevin448@gmail.com>
2017-08-19 17:17:53 -0700

    start mail worker

--- service/mail/conf/conf.go
+++ service/mail/conf/conf.go
@@ -12,5 +12,7 @@ func Conf(c *governor.Config) error {
 	v.SetDefault("mail.host", "localhost")
 	v.SetDefault("mail.port", "587")
 	v.SetDefault("mail.insecure", false)
+	v.SetDefault("mail.buffer_size", 1024)
+	v.SetDefault("mail.worker_size", 2)
 	return nil
 }
--- service/mail/mail.go
+++ service/mail/mail.go
@@ -7,6 +7,7 @@ import (
 	"github.com/sirupsen/logrus"
 	"gopkg.in/gomail.v2"
 	"net/http"
+	"time"
 )
 
 type (
@@ -17,8 +18,15 @@ type (
 	}
 
 	goMail struct {
-		mailer      *gomail.Dialer
+		host        string
+		port        int
+		username    string
+		password    string
+		insecure    bool
+		bufferSize  int
+		workerSize  int
 		fromAddress string
+		msgc        chan *gomail.Message
 	}
 )
 
@@ -31,19 +39,64 @@ func New(c governor.Config, l *logrus.Logger) Mail {
 	v := c.Conf()
 	rconf := v.GetStringMapString("mail")
 
-	m := gomail.NewDialer(rconf["host"], v.GetInt("mail.port"), rconf["username"], rconf["password"])
+	l.Info("initialized mail service")
 
-	if v.GetBool("mail.insecure") {
-		m.TLSConfig = &tls.Config{
+	return &goMail{
+		host:        rconf["host"],
+		port:        v.GetInt("mail.port"),
+		username:    rconf["username"],
+		password:    rconf["password"],
+		insecure:    v.GetBool("mail.insecure"),
+		bufferSize:  v.GetInt("mail.buffer_size"),
+		workerSize:  v.GetInt("mail.worker_size"),
+		fromAddress: rconf["from_address"],
+		msgc:        make(chan *gomail.Message),
+	}
+}
+
+func (m *goMail) dialer() *gomail.Dialer {
+	d := gomail.NewDialer(m.host, m.port, m.username, m.password)
+
+	if m.insecure {
+		d.TLSConfig = &tls.Config{
 			InsecureSkipVerify: true,
 		}
 	}
 
-	l.Info("initialized mail service")
+	return d
+}
 
-	return &goMail{
-		mailer:      m,
-		fromAddress: rconf["from_address"],
+func (m *goMail) mailWorker(l *logrus.Logger, ch <-chan *gomail.Message) {
+	d := m.dialer()
+
+	var s gomail.SendCloser
+	var err error
+	open := false
+	for {
+		select {
+		case m, ok := <-ch:
+			if !ok {
+				return
+			}
+			if !open {
+				if s, err = d.Dial(); err != nil {
+					panic(err)
+				}
+				open = true
+			}
+			if err := gomail.Send(s, m); err != nil {
+				l.Error(err)
+			}
+		// Close the connection to the SMTP server if no email was sent in
+		// the last 30 seconds.
+		case <-time.After(30 * time.Second):
+			if open {
+				if err := s.Close(); err != nil {
+					panic(err)
+				}
+				open = false
+			}
+		}
 	}
 }
 
@@ -75,9 +128,9 @@ func (m *goMail) Send(to, subject, body string) *governor.Error {
 	msg.SetHeader("Subject", subject)
 	msg.SetBody("text/html", body)
 
-	if err := m.mailer.DialAndSend(msg); err != nil {
-		return governor.NewError(moduleIDSend, err.Error(), 0, http.StatusInternalServerError)
-	}
+	//if err := m.mailer.DialAndSend(msg); err != nil {
+	//	return governor.NewError(moduleIDSend, err.Error(), 0, http.StatusInternalServerError)
+	//}
 
 	return nil
 }
