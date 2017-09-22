958d85275634b5c041a3059b96aeb3cd4346799e
Kevin Wang <wangkevin448@gmail.com>
2017-09-21 21:19:28 -0700

    session cookie system

--- service/user/auth.go
+++ service/user/auth.go
@@ -19,7 +19,7 @@ type (
 	reqUserAuth struct {
 		Username     string `json:"username"`
 		Password     string `json:"password"`
-		RefreshToken string `json:"refresh_token"`
+		SessionToken string `json:"session_token"`
 	}
 
 	reqExchangeToken struct {
@@ -30,6 +30,7 @@ type (
 		Valid        bool          `json:"valid"`
 		AccessToken  string        `json:"access_token,omitempty"`
 		RefreshToken string        `json:"refresh_token,omitempty"`
+		SessionToken string        `json:"session_token,omitempty"`
 		Claims       *token.Claims `json:"claims,omitempty"`
 	}
 )
@@ -54,6 +55,7 @@ func (r *reqExchangeToken) valid() *governor.Error {
 const (
 	authenticationSubject = "authentication"
 	refreshSubject        = "refresh"
+	sessionSubject        = "session"
 )
 
 func (u *userService) setAccessCookie(c echo.Context, conf governor.Config, accessToken string) {
@@ -94,6 +96,17 @@ func (u *userService) setRefreshCookie(c echo.Context, conf governor.Config, ref
 	})
 }
 
+func (u *userService) setSessionCookie(c echo.Context, conf governor.Config, sessionToken, userid string) {
+	ub64 := strings.TrimRight(userid, "=")
+	c.SetCookie(&http.Cookie{
+		Name:     "session_token_" + ub64,
+		Value:    sessionToken,
+		Path:     conf.BaseURL + "/u/auth/login",
+		MaxAge:   month6,
+		HttpOnly: false,
+	})
+}
+
 func getAccessCookie(c echo.Context) (string, error) {
 	cookie, err := c.Cookie("access_token")
 	if err != nil {
@@ -116,6 +129,21 @@ func getRefreshCookie(c echo.Context) (string, error) {
 	return cookie.Value, nil
 }
 
+func getSessionCookie(c echo.Context, userid string) (string, error) {
+	ub64 := strings.TrimRight(userid, "=")
+	if ub64 == "" {
+		return "", errors.New("no cookie value")
+	}
+	cookie, err := c.Cookie("session_token_" + ub64)
+	if err != nil {
+		return "", err
+	}
+	if cookie.Value == "" {
+		return "", errors.New("no cookie value")
+	}
+	return cookie.Value, nil
+}
+
 func rmAccessCookie(c echo.Context, conf governor.Config) {
 	c.SetCookie(&http.Cookie{
 		Name:   "access_token",
@@ -127,6 +155,12 @@ func rmAccessCookie(c echo.Context, conf governor.Config) {
 
 func rmRefreshCookie(c echo.Context, conf governor.Config) {
 	c.SetCookie(&http.Cookie{
+		Name:   "refresh_token",
+		Value:  "invalid",
+		MaxAge: -1,
+		Path:   conf.BaseURL + "/u/auth",
+	})
+	c.SetCookie(&http.Cookie{
 		Name:   "refresh_valid",
 		Value:  "invalid",
 		MaxAge: -1,
@@ -137,6 +171,16 @@ func rmRefreshCookie(c echo.Context, conf governor.Config) {
 		Value:  "invalid",
 		MaxAge: -1,
 		Path:   "/",
+	})
+}
+
+func rmSessionCookie(c echo.Context, conf governor.Config, userid string) {
+	ub64 := strings.TrimRight(userid, "=")
+	c.SetCookie(&http.Cookie{
+		Name:   "session_token_" + ub64,
+		Value:  "invalid",
+		MaxAge: -1,
+		Path:   conf.BaseURL + "/u/auth/login",
 	})
 }
 
@@ -164,41 +208,43 @@ func (u *userService) mountAuth(conf governor.Config, r *echo.Group, l *logrus.L
 		if err := c.Bind(&ruser); err != nil {
 			return governor.NewErrorUser(moduleIDAuth, err.Error(), 0, http.StatusBadRequest)
 		}
-		if t, err := getRefreshCookie(c); err == nil {
-			ruser.RefreshToken = t
-		}
 		if err := ruser.valid(); err != nil {
 			return err
 		}
-
 		m, err := usermodel.GetByUsername(db, ruser.Username)
 		if err != nil {
 			if err.Code() == 2 {
 				err.SetErrorUser()
 			}
+			err.AddTrace(moduleIDAuth)
 			return err
+		}
+		userid, err := m.IDBase64()
+		if err != nil {
+			err.AddTrace(moduleIDAuth)
+			return err
+		}
+		if t, err := getSessionCookie(c, userid); err == nil {
+			ruser.SessionToken = t
 		}
 
 		if m.ValidatePass(ruser.Password) {
 			sessionID := ""
-			userid := ""
 			isMember := false
 			// if claims userid matches model, session_id is provided,
-			// is in list of user sessions, and is valid, set it as the sessionID
+			// is in list of user sessions, set it as the sessionID
 			// the session can be expired by time
-			if ok, claims := u.tokenizer.GetClaims(ruser.RefreshToken, refreshSubject); ok {
-				if ub64, err := m.IDBase64(); err == nil && ub64 == claims.Userid {
-					if s := strings.Split(claims.Id, ":"); len(s) == 2 {
-						usersession := session.Session{
-							Userid: claims.Userid,
-						}
-						userkey := usersession.UserKey()
-						if isM, err := ch.HExists(userkey, s[0]).Result(); err == nil && isM {
-							if id, err := uid.FromBase64(4, 8, 4, s[0]); err == nil {
-								sessionID = id.Base64()
-								userid = claims.Userid
-								isMember = isM
-							}
+			if ok, claims := u.tokenizer.GetClaims(ruser.SessionToken, sessionSubject); ok {
+				if userid == claims.Userid {
+					usersession := session.Session{
+						Userid: claims.Userid,
+					}
+					userkey := usersession.UserKey()
+					if isM, err := ch.HExists(userkey, claims.Id).Result(); err == nil && isM {
+						sessionID = claims.Id
+						isMember = isM
+					} else {
+						if err != nil {
 						}
 					}
 				}
@@ -224,8 +270,14 @@ func (u *userService) mountAuth(conf governor.Config, r *echo.Group, l *logrus.L
 				err.AddTrace(moduleIDAuth)
 				return err
 			}
-			// generate a refresh tokens with the sessionKey
+			// generate a refresh token with the sessionKey
 			refreshToken, _, err := u.tokenizer.Generate(m, u.refreshTime, refreshSubject, s.SessionID+":"+s.SessionKey)
+			if err != nil {
+				err.AddTrace(moduleIDAuth)
+				return err
+			}
+			// generate a session token
+			sessionToken, _, err := u.tokenizer.Generate(m, u.refreshTime, sessionSubject, s.SessionID)
 			if err != nil {
 				err.AddTrace(moduleIDAuth)
 				return err
@@ -274,11 +326,13 @@ func (u *userService) mountAuth(conf governor.Config, r *echo.Group, l *logrus.L
 
 			u.setAccessCookie(c, conf, accessToken)
 			u.setRefreshCookie(c, conf, refreshToken, claims.AuthTags)
+			u.setSessionCookie(c, conf, sessionToken, userid)
 
 			return c.JSON(http.StatusOK, resUserAuth{
 				Valid:        true,
 				AccessToken:  accessToken,
 				RefreshToken: refreshToken,
+				SessionToken: sessionToken,
 				Claims:       claims,
 			})
 		}
@@ -369,6 +423,7 @@ func (u *userService) mountAuth(conf governor.Config, r *echo.Group, l *logrus.L
 
 		sessionID := ""
 		sessionKey := ""
+		userid := ""
 		// if session_id is provided, is in cache, and is valid, set it as the sessionID
 		// the session cannot be expired
 		if ok, claims := u.tokenizer.GetClaims(ruser.RefreshToken, refreshSubject); ok {
@@ -376,6 +431,7 @@ func (u *userService) mountAuth(conf governor.Config, r *echo.Group, l *logrus.L
 				if key, err := ch.Get(s[0]).Result(); err == nil {
 					sessionID = s[0]
 					sessionKey = key
+					userid = claims.Userid
 				}
 			}
 		}
@@ -407,12 +463,20 @@ func (u *userService) mountAuth(conf governor.Config, r *echo.Group, l *logrus.L
 			return err
 		}
 
+		// generate a new sessionToken from the refreshToken claims
+		sessionToken, err := u.tokenizer.GenerateFromClaims(claims, u.refreshTime, sessionSubject, sessionID)
+		if err != nil {
+			err.AddTrace(moduleIDAuth)
+			return err
+		}
+
 		// set the session id and key into cache
 		if err := ch.Set(sessionID, sessionKey, time.Duration(u.refreshTime*b1)).Err(); err != nil {
 			return governor.NewError(moduleIDAuth, err.Error(), 0, http.StatusInternalServerError)
 		}
 
 		u.setRefreshCookie(c, conf, refreshToken, claims.AuthTags)
+		u.setSessionCookie(c, conf, sessionToken, userid)
 
 		return c.JSON(http.StatusOK, resUserAuth{
 			Valid:        true,
