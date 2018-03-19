89fe28eebe4a74afe050097e699cc69f21f3ae4a
Kevin Wang <wangkevin448@gmail.com>
2018-03-19 15:29:13 -0700

    Implement websocket service methods

--- service/websocket/websocket.go
+++ service/websocket/websocket.go
@@ -14,18 +14,28 @@ const (
 type (
 	// Websocket is a websocket management service
 	Websocket interface {
-		Subscribe(channel string, ws string) *governor.Error
-		Unsubscribe(channel string, ws string) *governor.Error
-		Broadcast(channel string, msg string) *governor.Error
+		Subscribe(conn *Conn)
+		Unsubscribe(conn *Conn)
+		Broadcast(channel string, msg interface{}) *governor.Error
+		NewConn(channels []string, w http.ResponseWriter, r *http.Request) (*Conn, *governor.Error)
 	}
 
 	websocketService struct {
 		channels map[string]*channel
 		upgrader *websocket.Upgrader
+		logger   *logrus.Logger
 	}
 
 	channel struct {
-		listeners map[*websocket.Conn]bool
+		id        string
+		listeners map[*Conn]bool
+	}
+
+	// Conn describes a websocket connection subscribed to multiple channels
+	Conn struct {
+		w        Websocket
+		conn     *websocket.Conn
+		channels []string
 	}
 )
 
@@ -39,26 +49,100 @@ func New(conf governor.Config, l *logrus.Logger) Websocket {
 	}
 }
 
-func (w *websocketService) Subscribe(channel string, ws string) *governor.Error {
-	_, validChannel := w.channels[channel]
+func (w *websocketService) getChannel(channel string) *channel {
+	if c, ok := w.channels[channel]; ok {
+		return c
+	}
+	c := newChannel(channel)
+	w.channels[channel] = c
+	return c
+}
+
+func (w *websocketService) removeChannel(channel string) {
+	delete(w.channels, channel)
+}
+
+// Subscribe adds a websocket connection to a channel
+func (w *websocketService) Subscribe(conn *Conn) {
+	for _, channel := range conn.channels {
+		c := w.getChannel(channel)
+		c.listeners[conn] = true
+	}
+}
+
+// Unsubscribe removes a websocket connection from a channel
+func (w *websocketService) Unsubscribe(conn *Conn) {
+	for _, channel := range conn.channels {
+		if c, ok := w.channels[channel]; ok {
+			delete(c.listeners, conn)
+		}
+	}
+}
+
+const (
+	moduleIDBroadcast = moduleID + ".Broadcast"
+)
+
+// Broadcast sends a message to all websocket connections in a channel
+func (w *websocketService) Broadcast(channel string, msg interface{}) *governor.Error {
+	c, validChannel := w.channels[channel]
 	if !validChannel {
-		return governor.NewErrorUser(moduleID, "Invalid channel", 0, http.StatusNotFound)
+		return governor.NewError(moduleID, "Invalid channel", 2, http.StatusNotFound)
+	}
+	for listener := range c.listeners {
+		if err := listener.conn.WriteJSON(msg); err != nil {
+			w.logger.WithFields(logrus.Fields{
+				"origin": moduleIDBroadcast,
+			}).Errorf("Failed to send websocket message: %s", err.Error())
+		}
 	}
 	return nil
 }
 
-func (w *websocketService) Unsubscribe(channel string, ws string) *governor.Error {
-	_, validChannel := w.channels[channel]
-	if !validChannel {
-		return governor.NewErrorUser(moduleID, "Invalid channel", 0, http.StatusNotFound)
+func newChannel(id string) *channel {
+	return &channel{
+		id:        id,
+		listeners: make(map[*Conn]bool),
+	}
+}
+
+const (
+	moduleIDConn = moduleID + ".Conn"
+
+	moduleIDConnNew = moduleIDConn + ".New"
+)
+
+// NewConn establishes a new websocket connection from an existing http connection
+func (w *websocketService) NewConn(channels []string, rw http.ResponseWriter, r *http.Request) (*Conn, *governor.Error) {
+	ws, err := w.upgrader.Upgrade(rw, r, nil)
+	if err != nil {
+		return nil, governor.NewErrorUser(moduleIDConnNew, err.Error(), 0, http.StatusBadRequest)
+	}
+
+	c := &Conn{
+		w:        w,
+		conn:     ws,
+		channels: channels,
+	}
+	w.Subscribe(c)
+	return c, nil
+}
+
+const (
+	moduleIDConnRead = moduleIDConn + ".Read"
+)
+
+// Read reads a message to json
+func (c *Conn) Read(msg interface{}) *governor.Error {
+	if err := c.conn.ReadJSON(msg); err != nil {
+		return governor.NewError(moduleIDConnRead, err.Error(), 0, http.StatusInternalServerError)
 	}
 	return nil
 }
 
-func (w *websocketService) Broadcast(channel string, msg string) *governor.Error {
-	_, validChannel := w.channels[channel]
-	if !validChannel {
-		return governor.NewErrorUser(moduleID, "Invalid channel", 0, http.StatusNotFound)
+// Close closes the websocket connection
+func (c *Conn) Close() {
+	c.w.Unsubscribe(c)
+	if c.conn.Close() != nil {
 	}
-	return nil
 }
