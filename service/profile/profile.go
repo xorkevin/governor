bc539d86ab078d4ac0f66a67dae989201d7ab51a
Kevin Wang <wangkevin448@gmail.com>
2018-10-19 21:57:39 -0700

    Move profile service routes to separate service

--- service/profile/profile.go
+++ service/profile/profile.go
@@ -11,51 +11,7 @@ import (
 	"github.com/hackform/governor/service/user/gate"
 	"github.com/labstack/echo"
 	"github.com/sirupsen/logrus"
-	"io"
-	"net/http"
 )
-
-type (
-	reqProfileGetID struct {
-		Userid string `json:"userid"`
-	}
-
-	reqProfileModel struct {
-		Userid string `json:"-"`
-		Email  string `json:"contact_email"`
-		Bio    string `json:"bio"`
-	}
-
-	resProfileUpdate struct {
-		Userid string `json:"userid"`
-	}
-
-	resProfileModel struct {
-		Email string `json:"contact_email"`
-		Bio   string `json:"bio"`
-		Image string `json:"image"`
-	}
-)
-
-func (r *reqProfileGetID) valid() *governor.Error {
-	if err := hasUserid(r.Userid); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (r *reqProfileModel) valid() *governor.Error {
-	if err := hasUserid(r.Userid); err != nil {
-		return err
-	}
-	if err := validEmail(r.Email); err != nil {
-		return err
-	}
-	if err := validBio(r.Email); err != nil {
-		return err
-	}
-	return nil
-}
 
 const (
 	moduleID    = "profile"
@@ -78,6 +34,10 @@ type (
 		img  image.Image
 		cc   cachecontrol.CacheControl
 	}
+
+	profileRouter struct {
+		service profileService
+	}
 )
 
 // New creates a new Profile service
@@ -99,222 +59,21 @@ func New(conf governor.Config, l *logrus.Logger, db db.Database, obj objstore.Ob
 	}
 }
 
+func (p *profileService) newRouter() *profileRouter {
+	return &profileRouter{
+		service: *p,
+	}
+}
+
 // Mount is a collection of routes for accessing and modifying profile data
 func (p *profileService) Mount(conf governor.Config, r *echo.Group, l *logrus.Logger) error {
-	db := p.db.DB()
+	pr := p.newRouter()
 
-	r.POST("", func(c echo.Context) error {
-		rprofile := reqProfileModel{}
-		if err := c.Bind(&rprofile); err != nil {
-			return governor.NewErrorUser(moduleID, err.Error(), 0, http.StatusBadRequest)
-		}
-		rprofile.Userid = c.Get("userid").(string)
-		if err := rprofile.valid(); err != nil {
-			return err
-		}
-
-		m := profilemodel.Model{
-			Email: rprofile.Email,
-			Bio:   rprofile.Bio,
-		}
-
-		if err := m.SetIDB64(rprofile.Userid); err != nil {
-			err.SetErrorUser()
-			return err
-		}
-
-		if err := m.Insert(db); err != nil {
-			if err.Code() == 3 {
-				err.SetErrorUser()
-			}
-			err.AddTrace(moduleID)
-			return err
-		}
-
-		userid, _ := m.IDBase64()
-
-		return c.JSON(http.StatusCreated, resProfileUpdate{
-			Userid: userid,
-		})
-	}, gate.User(p.gate))
-
-	r.PUT("", func(c echo.Context) error {
-		rprofile := reqProfileModel{}
-		if err := c.Bind(&rprofile); err != nil {
-			return governor.NewErrorUser(moduleID, err.Error(), 0, http.StatusBadRequest)
-		}
-		rprofile.Userid = c.Get("userid").(string)
-		if err := rprofile.valid(); err != nil {
-			return err
-		}
-
-		m, err := profilemodel.GetByIDB64(db, rprofile.Userid)
-		if err != nil {
-			if err.Code() == 2 {
-				err.SetErrorUser()
-			}
-			err.AddTrace(moduleID)
-			return err
-		}
-
-		m.Email = rprofile.Email
-		m.Bio = rprofile.Bio
-
-		if err := m.Update(db); err != nil {
-			err.AddTrace(moduleID)
-			return err
-		}
-
-		return c.NoContent(http.StatusNoContent)
-	}, gate.User(p.gate))
-
-	r.PUT("/image", func(c echo.Context) error {
-		img := c.Get("image").(io.Reader)
-		imgSize := c.Get("imagesize").(int64)
-		thumb64 := c.Get("thumbnail").(string)
-		userid := c.Get("userid").(string)
-
-		m, err := profilemodel.GetByIDB64(db, userid)
-		if err != nil {
-			if err.Code() == 2 {
-				err.SetErrorUser()
-			}
-			err.AddTrace(moduleID)
-			return err
-		}
-
-		if err := p.obj.Put(userid+"-profile", image.MediaTypeJpeg, imgSize, img); err != nil {
-			err.AddTrace(moduleID)
-			return err
-		}
-
-		m.Image = thumb64
-		if err := m.Update(db); err != nil {
-			err.AddTrace(moduleID)
-			return err
-		}
-
-		return c.NoContent(http.StatusNoContent)
-	}, gate.User(p.gate), p.img.LoadJpeg("image", image.Options{
-		Width:          384,
-		Height:         384,
-		ThumbWidth:     32,
-		ThumbHeight:    32,
-		Quality:        85,
-		ThumbQuality:   85,
-		Crop:           true,
-		ContextField:   "image",
-		SizeField:      "imagesize",
-		ThumbnailField: "thumbnail",
-	}))
-
-	r.DELETE("/:id", func(c echo.Context) error {
-		rprofile := reqProfileGetID{
-			Userid: c.Param("id"),
-		}
-		if err := rprofile.valid(); err != nil {
-			return err
-		}
-
-		m, err := profilemodel.GetByIDB64(db, rprofile.Userid)
-		if err != nil {
-			if err.Code() == 2 {
-				err.SetErrorUser()
-			}
-			err.AddTrace(moduleID)
-			return err
-		}
-
-		if err := m.Delete(db); err != nil {
-			err.AddTrace(moduleID)
-			return err
-		}
-
-		return c.NoContent(http.StatusNoContent)
-	}, gate.OwnerOrAdmin(p.gate, "id"))
-
-	r.GET("", func(c echo.Context) error {
-		userid := c.Get("userid").(string)
-
-		m, err := profilemodel.GetByIDB64(db, userid)
-		if err != nil {
-			if err.Code() == 2 {
-				err.SetErrorUser()
-			}
-			err.AddTrace(moduleID)
-			return err
-		}
-
-		return c.JSON(http.StatusOK, resProfileModel{
-			Email: m.Email,
-			Bio:   m.Bio,
-			Image: m.Image,
-		})
-	}, gate.User(p.gate))
-
-	r.GET("/:id", func(c echo.Context) error {
-		rprofile := reqProfileGetID{
-			Userid: c.Param("id"),
-		}
-		if err := rprofile.valid(); err != nil {
-			return err
-		}
-
-		m, err := profilemodel.GetByIDB64(db, rprofile.Userid)
-		if err != nil {
-			if err.Code() == 2 {
-				err.SetErrorUser()
-			}
-			err.AddTrace(moduleID)
-			return err
-		}
-
-		return c.JSON(http.StatusOK, resProfileModel{
-			Email: m.Email,
-			Bio:   m.Bio,
-			Image: m.Image,
-		})
-	}, p.cc.Control(true, false, min15, nil))
-
-	r.GET("/:id/image", func(c echo.Context) error {
-		rprofile := reqProfileGetID{
-			Userid: c.Param("id"),
-		}
-		if err := rprofile.valid(); err != nil {
-			return err
-		}
-
-		obj, objinfo, err := p.obj.Get(rprofile.Userid + "-profile")
-		if err != nil {
-			if err.Code() == 2 {
-				err.SetErrorUser()
-			}
-			err.AddTrace(moduleID)
-			return err
-		}
-		return c.Stream(http.StatusOK, objinfo.ContentType, obj)
-	}, p.cc.Control(true, false, min15, func(c echo.Context) (string, *governor.Error) {
-		rprofile := reqProfileGetID{
-			Userid: c.Param("id"),
-		}
-		if err := rprofile.valid(); err != nil {
-			return "", err
-		}
-
-		objinfo, err := p.obj.Stat(rprofile.Userid + "-profile")
-		if err != nil {
-			if err.Code() == 2 {
-				err.SetErrorUser()
-			}
-			err.AddTrace(moduleID)
-			return "", err
-		}
-
-		return objinfo.ETag, nil
-	}))
+	if err := pr.mountProfileRoutes(conf, r); err != nil {
+		return err
+	}
 
 	l.Info("mounted profile service")
-
 	return nil
 }
 
---
+++ service/profile/route_profile.go
@@ -0,0 +1,276 @@
+package profile
+
+import (
+	"github.com/hackform/governor"
+	"github.com/hackform/governor/service/image"
+	"github.com/hackform/governor/service/profile/model"
+	"github.com/hackform/governor/service/user/gate"
+	"github.com/labstack/echo"
+	"io"
+	"net/http"
+)
+
+type (
+	reqProfileGetID struct {
+		Userid string `json:"userid"`
+	}
+
+	reqProfileModel struct {
+		Userid string `json:"-"`
+		Email  string `json:"contact_email"`
+		Bio    string `json:"bio"`
+	}
+
+	resProfileUpdate struct {
+		Userid string `json:"userid"`
+	}
+
+	resProfileModel struct {
+		Email string `json:"contact_email"`
+		Bio   string `json:"bio"`
+		Image string `json:"image"`
+	}
+)
+
+func (r *reqProfileGetID) valid() *governor.Error {
+	if err := hasUserid(r.Userid); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (r *reqProfileModel) valid() *governor.Error {
+	if err := hasUserid(r.Userid); err != nil {
+		return err
+	}
+	if err := validEmail(r.Email); err != nil {
+		return err
+	}
+	if err := validBio(r.Email); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (p *profileRouter) createProfile(c echo.Context) error {
+	rprofile := reqProfileModel{}
+	if err := c.Bind(&rprofile); err != nil {
+		return governor.NewErrorUser(moduleID, err.Error(), 0, http.StatusBadRequest)
+	}
+	rprofile.Userid = c.Get("userid").(string)
+	if err := rprofile.valid(); err != nil {
+		return err
+	}
+
+	m := profilemodel.Model{
+		Email: rprofile.Email,
+		Bio:   rprofile.Bio,
+	}
+
+	if err := m.SetIDB64(rprofile.Userid); err != nil {
+		err.SetErrorUser()
+		return err
+	}
+
+	if err := m.Insert(p.service.db.DB()); err != nil {
+		if err.Code() == 3 {
+			err.SetErrorUser()
+		}
+		err.AddTrace(moduleID)
+		return err
+	}
+
+	userid, _ := m.IDBase64()
+
+	return c.JSON(http.StatusCreated, resProfileUpdate{
+		Userid: userid,
+	})
+}
+
+func (p *profileRouter) updateProfile(c echo.Context) error {
+	rprofile := reqProfileModel{}
+	if err := c.Bind(&rprofile); err != nil {
+		return governor.NewErrorUser(moduleID, err.Error(), 0, http.StatusBadRequest)
+	}
+	rprofile.Userid = c.Get("userid").(string)
+	if err := rprofile.valid(); err != nil {
+		return err
+	}
+
+	m, err := profilemodel.GetByIDB64(p.service.db.DB(), rprofile.Userid)
+	if err != nil {
+		if err.Code() == 2 {
+			err.SetErrorUser()
+		}
+		err.AddTrace(moduleID)
+		return err
+	}
+
+	m.Email = rprofile.Email
+	m.Bio = rprofile.Bio
+
+	if err := m.Update(p.service.db.DB()); err != nil {
+		err.AddTrace(moduleID)
+		return err
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
+
+func (p *profileRouter) updateImage(c echo.Context) error {
+	img := c.Get("image").(io.Reader)
+	imgSize := c.Get("imagesize").(int64)
+	thumb64 := c.Get("thumbnail").(string)
+	userid := c.Get("userid").(string)
+
+	m, err := profilemodel.GetByIDB64(p.service.db.DB(), userid)
+	if err != nil {
+		if err.Code() == 2 {
+			err.SetErrorUser()
+		}
+		err.AddTrace(moduleID)
+		return err
+	}
+
+	if err := p.service.obj.Put(userid+"-profile", image.MediaTypeJpeg, imgSize, img); err != nil {
+		err.AddTrace(moduleID)
+		return err
+	}
+
+	m.Image = thumb64
+	if err := m.Update(p.service.db.DB()); err != nil {
+		err.AddTrace(moduleID)
+		return err
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
+
+func (p *profileRouter) deleteProfile(c echo.Context) error {
+	rprofile := reqProfileGetID{
+		Userid: c.Param("id"),
+	}
+	if err := rprofile.valid(); err != nil {
+		return err
+	}
+
+	m, err := profilemodel.GetByIDB64(p.service.db.DB(), rprofile.Userid)
+	if err != nil {
+		if err.Code() == 2 {
+			err.SetErrorUser()
+		}
+		err.AddTrace(moduleID)
+		return err
+	}
+
+	if err := m.Delete(p.service.db.DB()); err != nil {
+		err.AddTrace(moduleID)
+		return err
+	}
+
+	return c.NoContent(http.StatusNoContent)
+}
+
+func (p *profileRouter) getOwnProfile(c echo.Context) error {
+	userid := c.Get("userid").(string)
+
+	m, err := profilemodel.GetByIDB64(p.service.db.DB(), userid)
+	if err != nil {
+		if err.Code() == 2 {
+			err.SetErrorUser()
+		}
+		err.AddTrace(moduleID)
+		return err
+	}
+
+	return c.JSON(http.StatusOK, resProfileModel{
+		Email: m.Email,
+		Bio:   m.Bio,
+		Image: m.Image,
+	})
+}
+
+func (p *profileRouter) getProfile(c echo.Context) error {
+	rprofile := reqProfileGetID{
+		Userid: c.Param("id"),
+	}
+	if err := rprofile.valid(); err != nil {
+		return err
+	}
+
+	m, err := profilemodel.GetByIDB64(p.service.db.DB(), rprofile.Userid)
+	if err != nil {
+		if err.Code() == 2 {
+			err.SetErrorUser()
+		}
+		err.AddTrace(moduleID)
+		return err
+	}
+
+	return c.JSON(http.StatusOK, resProfileModel{
+		Email: m.Email,
+		Bio:   m.Bio,
+		Image: m.Image,
+	})
+}
+
+func (p *profileRouter) getProfileImage(c echo.Context) error {
+	rprofile := reqProfileGetID{
+		Userid: c.Param("id"),
+	}
+	if err := rprofile.valid(); err != nil {
+		return err
+	}
+
+	obj, objinfo, err := p.service.obj.Get(rprofile.Userid + "-profile")
+	if err != nil {
+		if err.Code() == 2 {
+			err.SetErrorUser()
+		}
+		err.AddTrace(moduleID)
+		return err
+	}
+	return c.Stream(http.StatusOK, objinfo.ContentType, obj)
+}
+
+func (p *profileRouter) getProfileImageCC(c echo.Context) (string, *governor.Error) {
+	rprofile := reqProfileGetID{
+		Userid: c.Param("id"),
+	}
+	if err := rprofile.valid(); err != nil {
+		return "", err
+	}
+
+	objinfo, err := p.service.obj.Stat(rprofile.Userid + "-profile")
+	if err != nil {
+		if err.Code() == 2 {
+			err.SetErrorUser()
+		}
+		err.AddTrace(moduleID)
+		return "", err
+	}
+
+	return objinfo.ETag, nil
+}
+
+func (p *profileRouter) mountProfileRoutes(conf governor.Config, r *echo.Group) error {
+	r.POST("", p.createProfile, gate.User(p.service.gate))
+	r.PUT("", p.updateProfile, gate.User(p.service.gate))
+	r.PUT("/image", p.updateImage, gate.User(p.service.gate), p.service.img.LoadJpeg("image", image.Options{
+		Width:          384,
+		Height:         384,
+		ThumbWidth:     32,
+		ThumbHeight:    32,
+		Quality:        85,
+		ThumbQuality:   85,
+		Crop:           true,
+		ContextField:   "image",
+		SizeField:      "imagesize",
+		ThumbnailField: "thumbnail",
+	}))
+	r.DELETE("/:id", p.deleteProfile, gate.OwnerOrAdmin(p.service.gate, "id"))
+	r.GET("", p.getOwnProfile, gate.User(p.service.gate))
+	r.GET("/:id", p.getProfile, p.service.cc.Control(true, false, min15, nil))
+	r.GET("/:id/image", p.getProfileImage, p.service.cc.Control(true, false, min15, p.getProfileImageCC))
+	return nil
+}
