7878b4256a9225465748f2240ebb8b26766ebd65
Kevin Wang <kevin@xorkevin.com>
2019-11-15 23:48:15 -0800

    Add valid email tests

---
+++ service/user/valid_test.go
@@ -0,0 +1,23 @@
+package user
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestValidEmail(t *testing.T) {
+	assert := assert.New(t)
+
+	assert.Nil(validEmail("gov@xorkevin.com"), "email should be valid")
+	assert.Nil(validEmail("gov@xor-kevin.com"), "hostname may contain dashes")
+	assert.Nil(validEmail("gov@tld"), "top level domain email should be valid")
+	assert.Nil(validEmail("gov+te.st@tld"), "+ and . are valid characters")
+	assert.Nil(validEmail("_gov+test-@tld"), "_ and - may begin and end local part")
+	assert.NotNil(validEmail("+gov@tld"), "+ may not begin local part")
+	assert.NotNil(validEmail("gov..test@tld"), "two . may not be adjacent in local part")
+	assert.NotNil(validEmail(".gov@tld"), ". may not begin local part")
+	assert.NotNil(validEmail("gov.@tld"), ". may not end local part")
+	assert.NotNil(validEmail("gov@-tld"), "- may not begin hostname part")
+	assert.NotNil(validEmail("gov@tld-"), "- may not end hostname part")
+	assert.NotNil(validEmail("gov@xor..kevin.com"), "two . may not be adjacent in hostname part")
+}
