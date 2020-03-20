8e79e52f57b975cf8a842b81985b7184a46c5901
Kevin Wang <kevin@xorkevin.com>
2020-03-19 18:59:56 -0700

    Impl apikey service

---
+++ service/user/apikey/apikey.go
@@ -0,0 +1,78 @@
+package apikey
+
+import (
+	"context"
+	"github.com/labstack/echo/v4"
+	"xorkevin.dev/governor"
+	"xorkevin.dev/governor/service/user/apikey/model"
+	"xorkevin.dev/governor/service/user/role"
+	"xorkevin.dev/governor/util/rank"
+)
+
+type (
+	// Apikey manages apikeys
+	Apikey interface {
+		GetUserKeys(userid string, limit, offset int) ([]apikeymodel.Model, error)
+		CheckKey(userid, keyid, key string, authtags rank.Rank) (bool, error)
+		Insert(userid string, authtags rank.Rank, name, desc string) (*ResApikeyModel, error)
+		RotateKey(keyid string) (*ResApikeyModel, error)
+		UpdateKey(userid, keyid string, authtags rank.Rank, name, desc string) error
+		DeleteKey(keyid string) error
+		DeleteUserKeys(userid string) error
+	}
+
+	Service interface {
+		governor.Service
+		Apikey
+	}
+
+	service struct {
+		apikeys apikeymodel.Repo
+		roles   role.Role
+		logger  governor.Logger
+	}
+)
+
+const (
+	time24h int64 = 86400
+	b1            = 1_000_000_000
+)
+
+// New returns a new Apikey
+func New(apikeys apikeymodel.Repo, roles role.Role) Service {
+	return &service{
+		apikeys: apikeys,
+		roles:   roles,
+	}
+}
+
+func (s *service) Register(r governor.ConfigRegistrar, jr governor.JobRegistrar) {
+}
+
+func (s *service) Init(ctx context.Context, c governor.Config, r governor.ConfigReader, l governor.Logger, g *echo.Group) error {
+	return nil
+}
+
+func (s *service) Setup(req governor.ReqSetup) error {
+	l := s.logger.WithData(map[string]string{
+		"phase": "setup",
+	})
+
+	if err := s.apikeys.Setup(); err != nil {
+		return err
+	}
+	l.Info("created user apikey table", nil)
+
+	return nil
+}
+
+func (s *service) Start(ctx context.Context) error {
+	return nil
+}
+
+func (s *service) Stop(ctx context.Context) {
+}
+
+func (s *service) Health() error {
+	return nil
+}
---
+++ service/user/apikey/service_apikey.go
@@ -0,0 +1,129 @@
+package apikey
+
+import (
+	"net/http"
+	"xorkevin.dev/governor"
+	"xorkevin.dev/governor/service/user/apikey/model"
+	"xorkevin.dev/governor/util/rank"
+)
+
+func (s *service) GetUserKeys(userid string, limit, offset int) ([]apikeymodel.Model, error) {
+	return s.apikeys.GetUserKeys(userid, limit, offset)
+}
+
+func (s *service) CheckKey(userid, keyid, key string, authtags rank.Rank) (bool, error) {
+	m, err := s.apikeys.GetByID(keyid)
+	if err != nil {
+		if governor.ErrorStatus(err) == http.StatusNotFound {
+			return false, governor.NewError("Invalid key", http.StatusUnauthorized, nil)
+		}
+		return false, err
+	}
+	if ok, err := s.apikeys.ValidateKey(key, m); err != nil || !ok {
+		return false, governor.NewError("Invalid key", http.StatusUnauthorized, nil)
+	}
+	if authtags == nil || authtags.Len() == 0 {
+		return true, nil
+	}
+
+	if inter := m.AuthTags.Intersect(authtags); inter.Len() != authtags.Len() {
+		return false, governor.NewError("User is forbidden", http.StatusForbidden, nil)
+	}
+	if inter, err := s.roles.IntersectRoles(userid, authtags); err != nil {
+		return false, err
+	} else if inter.Len() != authtags.Len() {
+		return false, governor.NewError("User is forbidden", http.StatusForbidden, nil)
+	}
+	return true, nil
+}
+
+type (
+	ResApikeyModel struct {
+		Keyid string `json:"keyid"`
+		Key   string `json:"key"`
+	}
+)
+
+func (s *service) Insert(userid string, authtags rank.Rank, name, desc string) (*ResApikeyModel, error) {
+	intersect, err := s.roles.IntersectRoles(userid, authtags)
+	if err != nil {
+		return nil, err
+	}
+	m, key, err := s.apikeys.New(userid, intersect, name, desc)
+	if err != nil {
+		return nil, err
+	}
+	if err := s.apikeys.Insert(m); err != nil {
+		if governor.ErrorStatus(err) == http.StatusBadRequest {
+			return nil, governor.NewErrorUser("", 0, err)
+		}
+		return nil, err
+	}
+	return &ResApikeyModel{
+		Keyid: m.Keyid,
+		Key:   key,
+	}, nil
+}
+
+func (s *service) RotateKey(keyid string) (*ResApikeyModel, error) {
+	m, err := s.apikeys.GetByID(keyid)
+	if err != nil {
+		if governor.ErrorStatus(err) == http.StatusNotFound {
+			return nil, governor.NewErrorUser("", 0, err)
+		}
+		return nil, err
+	}
+	key, err := s.apikeys.RehashKey(m)
+	if err != nil {
+		return nil, err
+	}
+	if err := s.apikeys.Update(m); err != nil {
+		return nil, err
+	}
+	return &ResApikeyModel{
+		Keyid: m.Keyid,
+		Key:   key,
+	}, nil
+}
+
+func (s *service) UpdateKey(userid, keyid string, authtags rank.Rank, name, desc string) error {
+	m, err := s.apikeys.GetByID(keyid)
+	if err != nil {
+		if governor.ErrorStatus(err) == http.StatusNotFound {
+			return governor.NewErrorUser("", 0, err)
+		}
+		return err
+	}
+	intersect, err := s.roles.IntersectRoles(userid, authtags)
+	if err != nil {
+		return err
+	}
+	m.AuthTags = intersect
+	m.Name = name
+	m.Desc = desc
+	if err := s.apikeys.Update(m); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *service) DeleteKey(keyid string) error {
+	m, err := s.apikeys.GetByID(keyid)
+	if err != nil {
+		if governor.ErrorStatus(err) == http.StatusNotFound {
+			return governor.NewErrorUser("", 0, err)
+		}
+		return err
+	}
+	if err := s.apikeys.Delete(m); err != nil {
+		return err
+	}
+	return nil
+}
+
+func (s *service) DeleteUserKeys(userid string) error {
+	if err := s.apikeys.DeleteUserKeys(userid); err != nil {
+		return err
+	}
+	return nil
+}
