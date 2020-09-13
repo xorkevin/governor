c67869f227f612656977135f94cb5fa5c9c32f50
Kevin Wang <kevin@xorkevin.com>
2020-09-13 13:41:41 -0700

    Impl oauth app session model

--- service/user/oauth/model/model.go
+++ service/user/oauth/model/model.go
@@ -10,6 +10,7 @@ import (
 )
 
 //go:generate forge model -m Model -t oauthapps -p oauthapp -o model_gen.go Model
+//go:generate forge model -m SessionModel -t oauthsessions -p session -o session_model_gen.go SessionModel
 
 const (
 	uidSize = 16
@@ -20,7 +21,10 @@ type (
 	// Repo is an OAuthApp repository
 	Repo interface {
 		New(name, url, callbackURL string) (*Model, string, error)
+		ValidateKey(key string, m *Model) (bool, error)
+		RehashKey(m *Model) (string, error)
 		GetByID(clientid string) (*Model, error)
+		GetApps(limit, offset int) ([]Model, error)
 		Insert(m *Model) error
 		Update(m *Model) error
 		Delete(m *Model) error
@@ -41,6 +45,7 @@ type (
 		RedirectURI  string `model:"redirect_uri,VARCHAR(2047) NOT NULL" query:"redirect_uri"`
 		Logo         string `model:"logo,VARCHAR(4095)" query:"logo"`
 		KeyHash      string `model:"keyhash,VARCHAR(255) NOT NULL" query:"keyhash"`
+		Time         int64  `model:"time,BIGINT NOT NULL" query:"time,getgroup"`
 		CreationTime int64  `model:"creation_time,BIGINT NOT NULL" query:"creation_time"`
 	}
 )
@@ -82,8 +87,33 @@ func (r *repo) New(name, url, redirectURI string) (*Model, string, error) {
 		URL:          url,
 		RedirectURI:  redirectURI,
 		KeyHash:      hash,
+		Time:         now,
 		CreationTime: now,
 	}, keystr, nil
+}
+
+func (r *repo) ValidateKey(key string, m *Model) (bool, error) {
+	ok, err := r.verifier.Verify(key, m.KeyHash)
+	if err != nil {
+		return false, governor.NewError("Failed to verify key", http.StatusInternalServerError, err)
+	}
+	return ok, nil
+}
+
+func (r *repo) RehashKey(m *Model) (string, error) {
+	key, err := uid.New(keySize)
+	if err != nil {
+		return "", governor.NewError("Failed to create oauth client secret", http.StatusInternalServerError, err)
+	}
+	keystr := key.Base64()
+	keyhash, err := r.hasher.Hash(keystr)
+	if err != nil {
+		return "", governor.NewError("Failed to hash oauth client secret", http.StatusInternalServerError, err)
+	}
+	now := time.Now().Round(0).Unix()
+	m.KeyHash = keyhash
+	m.Time = now
+	return keystr, nil
 }
 
 func (r *repo) GetByID(clientid string) (*Model, error) {
@@ -97,6 +127,18 @@ func (r *repo) GetByID(clientid string) (*Model, error) {
 			return nil, governor.NewError("No OAuth app found with that id", http.StatusNotFound, err)
 		}
 		return nil, governor.NewError("Failed to get OAuth app config", http.StatusInternalServerError, err)
+	}
+	return m, nil
+}
+
+func (r *repo) GetApps(limit, offset int) ([]Model, error) {
+	db, err := r.db.DB()
+	if err != nil {
+		return nil, err
+	}
+	m, err := oauthappModelGetModelOrdTime(db, false, limit, offset)
+	if err != nil {
+		return nil, governor.NewError("Failed to get OAuth app configs", http.StatusInternalServerError, err)
 	}
 	return m, nil
 }
@@ -144,6 +186,184 @@ func (r *repo) Setup() error {
 	}
 	if err := oauthappModelSetup(db); err != nil {
 		return governor.NewError("Failed to setup OAuth app model", http.StatusInternalServerError, err)
+	}
+	return nil
+}
+
+type (
+	// SessionRepo is the OAuth session repository
+	SessionRepo interface {
+		New(userid, clientid, scope string) (*SessionModel, string, error)
+		ValidateCode(code string, m *SessionModel) (bool, error)
+		RehashCode(m *SessionModel) (string, error)
+		GetByID(userid, clientid string) (*SessionModel, error)
+		GetUserSessions(userid string, limit, offset int) ([]SessionModel, error)
+		Insert(m *SessionModel) error
+		Update(m *SessionModel) error
+		Delete(userid string, clientids []string) error
+		DeleteUserSessions(userid string) error
+		Setup() error
+	}
+
+	srepo struct {
+		db       db.Database
+		hasher   *hunter2.Blake2bHasher
+		verifier *hunter2.Verifier
+	}
+
+	// SessionModel is an connected OAuth session to a user account
+	SessionModel struct {
+		Userid       string `model:"userid,VARCHAR(31);index" query:"userid,deleq,userid"`
+		ClientID     string `model:"clientid,VARCHAR(31), PRIMARY KEY (userid, clientid);index" query:"clientid,getoneeq,userid,clientid;updeq,userid,clientid;deleq,userid,clientid|arr"`
+		Scope        string `model:"scope,VARCHAR(4095) NOT NULL" query:"scope"`
+		CodeHash     string `model:"codehash,VARCHAR(31) NOT NULL" query:"codehash"`
+		Time         int64  `model:"time,BIGINT NOT NULL" query:"time,getgroupeq,userid"`
+		LastAuthTime int64  `model:"auth_time,BIGINT NOT NULL" query:"auth_time"`
+		CreationTime int64  `model:"creation_time,BIGINT NOT NULL" query:"creation_time"`
+	}
+)
+
+// NewSessionRepo creates a new OAuth session repository
+func NewSessionRepo(database db.Database) SessionRepo {
+	hasher := hunter2.NewBlake2bHasher()
+	verifier := hunter2.NewVerifier()
+	verifier.RegisterHash(hasher)
+
+	return &srepo{
+		db:       database,
+		hasher:   hasher,
+		verifier: verifier,
+	}
+}
+
+func (r *srepo) New(userid, clientid, scope string) (*SessionModel, string, error) {
+	code, err := uid.New(keySize)
+	if err != nil {
+		return nil, "", governor.NewError("Failed to create oauth session code", http.StatusInternalServerError, err)
+	}
+	codestr := code.Base64()
+	codehash, err := r.hasher.Hash(codestr)
+	if err != nil {
+		return nil, "", governor.NewError("Failed to hash oauth session code", http.StatusInternalServerError, err)
+	}
+
+	now := time.Now().Round(0).Unix()
+	return &SessionModel{
+		Userid:       userid,
+		ClientID:     clientid,
+		Scope:        scope,
+		CodeHash:     codehash,
+		Time:         now,
+		LastAuthTime: now,
+		CreationTime: now,
+	}, codestr, nil
+}
+
+func (r *srepo) ValidateCode(code string, m *SessionModel) (bool, error) {
+	ok, err := r.verifier.Verify(code, m.CodeHash)
+	if err != nil {
+		return false, governor.NewError("Failed to verify code", http.StatusInternalServerError, err)
+	}
+	return ok, nil
+}
+
+func (r *srepo) RehashCode(m *SessionModel) (string, error) {
+	code, err := uid.New(keySize)
+	if err != nil {
+		return "", governor.NewError("Failed to create oauth session code", http.StatusInternalServerError, err)
+	}
+	codestr := code.Base64()
+	codehash, err := r.hasher.Hash(codestr)
+	if err != nil {
+		return "", governor.NewError("Failed to hash oauth session code", http.StatusInternalServerError, err)
+	}
+	now := time.Now().Round(0).Unix()
+	m.CodeHash = codehash
+	m.Time = now
+	return codestr, nil
+}
+
+func (r *srepo) GetByID(userid, clientid string) (*SessionModel, error) {
+	db, err := r.db.DB()
+	if err != nil {
+		return nil, err
+	}
+	m, code, err := sessionModelGetSessionModelEqUseridEqClientID(db, userid, clientid)
+	if err != nil {
+		if code == 2 {
+			return nil, governor.NewError("No OAuth session found with that id", http.StatusNotFound, err)
+		}
+		return nil, governor.NewError("Failed to get OAuth session", http.StatusInternalServerError, err)
+	}
+	return m, nil
+}
+
+func (r *srepo) GetUserSessions(userid string, limit, offset int) ([]SessionModel, error) {
+	db, err := r.db.DB()
+	if err != nil {
+		return nil, err
+	}
+	m, err := sessionModelGetSessionModelEqUseridOrdTime(db, userid, false, limit, offset)
+	if err != nil {
+		return nil, governor.NewError("Failed to get OAuth sessions", http.StatusInternalServerError, err)
+	}
+	return m, nil
+}
+
+func (r *srepo) Insert(m *SessionModel) error {
+	db, err := r.db.DB()
+	if err != nil {
+		return err
+	}
+	if code, err := sessionModelInsert(db, m); err != nil {
+		if code == 3 {
+			return governor.NewError("OAuth session already exists", http.StatusBadRequest, err)
+		}
+		return governor.NewError("Failed to insert OAuth session", http.StatusInternalServerError, err)
+	}
+	return nil
+}
+
+func (r *srepo) Update(m *SessionModel) error {
+	db, err := r.db.DB()
+	if err != nil {
+		return err
+	}
+	if _, err := sessionModelUpdSessionModelEqUseridEqClientID(db, m, m.Userid, m.ClientID); err != nil {
+		return governor.NewError("Failed to update OAuth session", http.StatusInternalServerError, err)
+	}
+	return nil
+}
+
+func (r *srepo) Delete(userid string, clientids []string) error {
+	db, err := r.db.DB()
+	if err != nil {
+		return err
+	}
+	if err := sessionModelDelEqUseridHasClientID(db, userid, clientids); err != nil {
+		return governor.NewError("Failed to delete OAuth session", http.StatusInternalServerError, err)
+	}
+	return nil
+}
+
+func (r *srepo) DeleteUserSessions(userid string) error {
+	db, err := r.db.DB()
+	if err != nil {
+		return err
+	}
+	if err := sessionModelDelEqUserid(db, userid); err != nil {
+		return governor.NewError("Failed to delete OAuth sessions", http.StatusInternalServerError, err)
+	}
+	return nil
+}
+
+func (r *srepo) Setup() error {
+	db, err := r.db.DB()
+	if err != nil {
+		return err
+	}
+	if err := sessionModelSetup(db); err != nil {
+		return governor.NewError("Failed to setup OAuth session model", http.StatusInternalServerError, err)
 	}
 	return nil
 }
--- service/user/oauth/model/model_gen.go
+++ service/user/oauth/model/model_gen.go
@@ -14,7 +14,7 @@ const (
 )
 
 func oauthappModelSetup(db *sql.DB) error {
-	_, err := db.Exec("CREATE TABLE IF NOT EXISTS oauthapps (clientid VARCHAR(31) PRIMARY KEY, name VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, redirect_uri VARCHAR(2047) NOT NULL, logo VARCHAR(4095), keyhash VARCHAR(255) NOT NULL, creation_time BIGINT NOT NULL);")
+	_, err := db.Exec("CREATE TABLE IF NOT EXISTS oauthapps (clientid VARCHAR(31) PRIMARY KEY, name VARCHAR(255) NOT NULL, url VARCHAR(255) NOT NULL, redirect_uri VARCHAR(2047) NOT NULL, logo VARCHAR(4095), keyhash VARCHAR(255) NOT NULL, time BIGINT NOT NULL, creation_time BIGINT NOT NULL);")
 	if err != nil {
 		return err
 	}
@@ -22,7 +22,7 @@ func oauthappModelSetup(db *sql.DB) error {
 }
 
 func oauthappModelInsert(db *sql.DB, m *Model) (int, error) {
-	_, err := db.Exec("INSERT INTO oauthapps (clientid, name, url, redirect_uri, logo, keyhash, creation_time) VALUES ($1, $2, $3, $4, $5, $6, $7);", m.ClientID, m.Name, m.URL, m.RedirectURI, m.Logo, m.KeyHash, m.CreationTime)
+	_, err := db.Exec("INSERT INTO oauthapps (clientid, name, url, redirect_uri, logo, keyhash, time, creation_time) VALUES ($1, $2, $3, $4, $5, $6, $7, $8);", m.ClientID, m.Name, m.URL, m.RedirectURI, m.Logo, m.KeyHash, m.Time, m.CreationTime)
 	if err != nil {
 		if postgresErr, ok := err.(*pq.Error); ok {
 			switch postgresErr.Code {
@@ -42,13 +42,13 @@ func oauthappModelInsertBulk(db *sql.DB, models []*Model, allowConflict bool) (i
 		conflictSQL = " ON CONFLICT DO NOTHING"
 	}
 	placeholders := make([]string, 0, len(models))
-	args := make([]interface{}, 0, len(models)*7)
+	args := make([]interface{}, 0, len(models)*8)
 	for c, m := range models {
-		n := c * 7
-		placeholders = append(placeholders, fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d)", n+1, n+2, n+3, n+4, n+5, n+6, n+7))
-		args = append(args, m.ClientID, m.Name, m.URL, m.RedirectURI, m.Logo, m.KeyHash, m.CreationTime)
+		n := c * 8
+		placeholders = append(placeholders, fmt.Sprintf("($%d, $%d, $%d, $%d, $%d, $%d, $%d, $%d)", n+1, n+2, n+3, n+4, n+5, n+6, n+7, n+8))
+		args = append(args, m.ClientID, m.Name, m.URL, m.RedirectURI, m.Logo, m.KeyHash, m.Time, m.CreationTime)
 	}
-	_, err := db.Exec("INSERT INTO oauthapps (clientid, name, url, redirect_uri, logo, keyhash, creation_time) VALUES "+strings.Join(placeholders, ", ")+conflictSQL+";", args...)
+	_, err := db.Exec("INSERT INTO oauthapps (clientid, name, url, redirect_uri, logo, keyhash, time, creation_time) VALUES "+strings.Join(placeholders, ", ")+conflictSQL+";", args...)
 	if err != nil {
 		if postgresErr, ok := err.(*pq.Error); ok {
 			switch postgresErr.Code {
@@ -64,7 +64,7 @@ func oauthappModelInsertBulk(db *sql.DB, models []*Model, allowConflict bool) (i
 
 func oauthappModelGetModelEqClientID(db *sql.DB, clientid string) (*Model, int, error) {
 	m := &Model{}
-	if err := db.QueryRow("SELECT clientid, name, url, redirect_uri, logo, keyhash, creation_time FROM oauthapps WHERE clientid = $1;", clientid).Scan(&m.ClientID, &m.Name, &m.URL, &m.RedirectURI, &m.Logo, &m.KeyHash, &m.CreationTime); err != nil {
+	if err := db.QueryRow("SELECT clientid, name, url, redirect_uri, logo, keyhash, time, creation_time FROM oauthapps WHERE clientid = $1;", clientid).Scan(&m.ClientID, &m.Name, &m.URL, &m.RedirectURI, &m.Logo, &m.KeyHash, &m.Time, &m.CreationTime); err != nil {
 		if err == sql.ErrNoRows {
 			return nil, 2, err
 		}
@@ -82,7 +82,7 @@ func oauthappModelGetModelEqClientID(db *sql.DB, clientid string) (*Model, int, 
 }
 
 func oauthappModelUpdModelEqClientID(db *sql.DB, m *Model, clientid string) (int, error) {
-	_, err := db.Exec("UPDATE oauthapps SET (clientid, name, url, redirect_uri, logo, keyhash, creation_time) = ROW($1, $2, $3, $4, $5, $6, $7) WHERE clientid = $8;", m.ClientID, m.Name, m.URL, m.RedirectURI, m.Logo, m.KeyHash, m.CreationTime, clientid)
+	_, err := db.Exec("UPDATE oauthapps SET (clientid, name, url, redirect_uri, logo, keyhash, time, creation_time) = ROW($1, $2, $3, $4, $5, $6, $7, $8) WHERE clientid = $9;", m.ClientID, m.Name, m.URL, m.RedirectURI, m.Logo, m.KeyHash, m.Time, m.CreationTime, clientid)
 	if err != nil {
 		if postgresErr, ok := err.(*pq.Error); ok {
 			switch postgresErr.Code {
@@ -99,4 +99,31 @@ func oauthappModelUpdModelEqClientID(db *sql.DB, m *Model, clientid string) (int
 func oauthappModelDelEqClientID(db *sql.DB, clientid string) error {
 	_, err := db.Exec("DELETE FROM oauthapps WHERE clientid = $1;", clientid)
 	return err
+}
+
+func oauthappModelGetModelOrdTime(db *sql.DB, orderasc bool, limit, offset int) ([]Model, error) {
+	order := "DESC"
+	if orderasc {
+		order = "ASC"
+	}
+	res := make([]Model, 0, limit)
+	rows, err := db.Query("SELECT clientid, name, url, redirect_uri, logo, keyhash, time, creation_time FROM oauthapps ORDER BY time "+order+" LIMIT $1 OFFSET $2;", limit, offset)
+	if err != nil {
+		return nil, err
+	}
+	defer func() {
+		if err := rows.Close(); err != nil {
+		}
+	}()
+	for rows.Next() {
+		m := Model{}
+		if err := rows.Scan(&m.ClientID, &m.Name, &m.URL, &m.RedirectURI, &m.Logo, &m.KeyHash, &m.Time, &m.CreationTime); err != nil {
+			return nil, err
+		}
+		res = append(res, m)
+	}
+	if err := rows.Err(); err != nil {
+		return nil, err
+	}
+	return res, nil
 }
