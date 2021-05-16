8ee4db0d5ac9b470347af5817d7f4c6ea5d0face
Kevin Wang <kevin@xorkevin.com>
2021-05-15 18:32:52 -0700

    Impl postgres register

--- example/anvil/postgres/init.sh
+++ example/anvil/postgres/init.sh
@@ -6,38 +6,25 @@ export ROOT_DIR=${0%/*}
 
 . "${ROOT_DIR}/_init_lib.sh"
 
-log2 'begin genpass'
+log2 'begin getpass'
 
 pass=
 
 while true; do
   export VAULT_TOKEN=
   i=0
-  while [ $i -lt "${CURL_REAUTH:-12}" ]; do
+  while [ $i -lt "${CURL_REAUTH:-3}" ]; do
     if [ -z $VAULT_TOKEN ]; then
       export VAULT_TOKEN=$(auth_vault)
       log2 'authenticate with vault'
     fi
 
-    status=$(vault_kvget "$KV_PATH")
+    status=$(vault_kvget "$KV_MOUNT" "$KV_PATH")
     if is_success "$status"; then
       log2 'found existing vault credentials'
       pass=$(cat /tmp/curlres.txt | jq -r '.data.data.password')
       log2 'use existing password'
       break 2
-    elif [ "$status" = 404 ]; then
-      log2 'no existing vault credentials'
-      if [ -z "$pass" ]; then
-        pass=$(gen_pass "${PASS_LEN:-64}")
-        log2 'generate new password'
-      fi
-
-      status=$(vault_kvput "$KV_PATH" "{\"username\": \"postgres\", \"password\": \"${pass}\"}" 0)
-      if is_success "$status"; then
-        log2 'write password to vault kv'
-        break 2
-      fi
-      log2 'error write password to vault kv:' "$(cat /tmp/curlres.txt)"
     else
       log2 'error get vault kv:' "$(cat /tmp/curlres.txt)"
     fi
---
+++ example/anvil/postgres/register/rolerevoke.sql
@@ -0,0 +1,3 @@
+REASSIGN OWNED BY "{{`{{name}}`}}" TO postgres;
+DROP OWNED BY "{{`{{name}}`}}";
+DROP ROLE IF EXISTS "{{`{{name}}`}}";
