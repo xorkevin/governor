92a73bbf990fb29523982d922e4627f68b6552cd
Kevin Wang <kevin@xorkevin.com>
2021-09-19 17:51:16 -0700

    Scaffold mailing list service

--- service/mailinglist/mailinglist.go
+++ service/mailinglist/mailinglist.go
@@ -5,11 +5,24 @@ import (
 	"time"
 
 	"xorkevin.dev/governor"
+	"xorkevin.dev/governor/service/objstore"
 	"xorkevin.dev/governor/util/bytefmt"
 )
 
 type (
+	// MailingList is a mailing list service
+	MailingList interface {
+	}
+
+	// Service is a MailingList and governor.Service
+	Service interface {
+		MailingList
+		governor.Service
+	}
+
 	service struct {
+		mailBucket   objstore.Bucket
+		rcvMailDir   objstore.Dir
 		logger       governor.Logger
 		port         string
 		domain       string
@@ -17,9 +30,34 @@ type (
 		readtimeout  time.Duration
 		writetimeout time.Duration
 	}
+
+	ctxKeyMailingList struct{}
 )
 
+// GetCtxMailingList returns a MailingList service from the context
+func GetCtxMailer(inj governor.Injector) MailingList {
+	v := inj.Get(ctxKeyMailingList{})
+	if v == nil {
+		return nil
+	}
+	return v.(MailingList)
+}
+
+// setCtxMailingList sets a MailingList service in the context
+func setCtxMailingList(inj governor.Injector, m MailingList) {
+	inj.Set(ctxKeyMailingList{}, m)
+}
+
+func New(obj objstore.Bucket) Service {
+	return &service{
+		mailBucket: obj,
+		rcvMailDir: obj.Subdir("rcvmail"),
+	}
+}
+
 func (s *service) Register(inj governor.Injector, r governor.ConfigRegistrar, jr governor.JobRegistrar) {
+	setCtxMailingList(inj, s)
+
 	r.SetDefault("port", "2525")
 	r.SetDefault("domain", "localhost")
 	r.SetDefault("maxmsgsize", "2M")
@@ -58,5 +96,31 @@ func (s *service) Init(ctx context.Context, c governor.Config, r governor.Config
 		"read timeout":       r.GetStr("readtimeout"),
 		"write timeout":      r.GetStr("writetimeout"),
 	})
+	return nil
+}
+
+func (s *service) Setup(req governor.ReqSetup) error {
+	l := s.logger.WithData(map[string]string{
+		"phase": "setup",
+	})
+	if err := s.mailBucket.Init(); err != nil {
+		return governor.ErrWithMsg(err, "Failed to init mail bucket")
+	}
+	l.Info("Created mail bucket", nil)
+	return nil
+}
+
+func (s *service) PostSetup(req governor.ReqSetup) error {
+	return nil
+}
+
+func (s *service) Start(ctx context.Context) error {
+	return nil
+}
+
+func (s *service) Stop(ctx context.Context) {
+}
+
+func (s *service) Health() error {
 	return nil
 }
