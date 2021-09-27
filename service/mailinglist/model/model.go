aba1a4d11167ef7618238ab8aec4c5b3f2a81cc4
Kevin Wang <kevin@xorkevin.com>
2021-09-27 01:02:28 -0700

    Scaffold mailinglist model

--- service/mailinglist/model/model.go
+++ service/mailinglist/model/model.go
@@ -3,7 +3,26 @@ package model
 type (
 	// ListModel is the db mailing list model
 	ListModel struct {
-		CreatorID string `model:"creatorid,VARCHAR(31)"`
-		ListID    string `model:"listid,VARCHAR(255)"`
+		ListID       string `model:"listid,VARCHAR(255) PRIMARY KEY" query:"listid"`
+		CreatorID    string `model:"creatorid,VARCHAR(31) NOT NULL" query:"creatorid"`
+		Name         string `model:"name,VARCHAR(255) NOT NULL" query:"name"`
+		Description  string `model:"description,VARCHAR(255)" query:"description"`
+		LastUpdated  int64  `model:"last_updated,BIGINT NOT NULL" query:"last_updated" query:"last_updated"`
+		CreationTime int64  `model:"creation_time,BIGINT NOT NULL" query:"creation_time" query:"creation_time"`
+	}
+
+	// MemberModel is the db mailing list member model
+	MemberModel struct {
+		ListID      string `model:"listid,VARCHAR(255)" query:"listid"`
+		Userid      string `model:"userid,VARCHAR(31), PRIMARY KEY (listid, userid)" query:"userid"`
+		LastUpdated int64  `model:"last_updated,BIGINT NOT NULL;index,userid" query:"last_updated"`
+	}
+
+	// MailModel is the db mailing list mail model
+	MailModel struct {
+		ListID       string `model:"listid,VARCHAR(255)" query:"listid"`
+		Msgid        string `model:"msgid,VARCHAR(1023), PRIMARY KEY (listid, msgid)" query:"msgid"`
+		Userid       string `model:"userid,VARCHAR(31) NOT NULL" query:"userid"`
+		CreationTime int64  `model:"creation_time,BIGINT NOT NULL;index,creatorid,listid" query:"creation_time"`
 	}
 )
