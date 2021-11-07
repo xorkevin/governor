13398ca3754bf7613e7f8ad7d5e742697bd33eae
Kevin Wang <kevin@xorkevin.com>
2021-11-07 00:56:10 -0700

    Add dkim test key

--- example/devtools/mailinglist/sendtest.sh
+++ example/devtools/mailinglist/sendtest.sh
@@ -1,3 +1,10 @@
 #!/bin/sh
 
-cat ./devtools/mailinglist/testmail.eml | mailcat fmt | mailcat send -s localhost:2525 -i 'return-path@mail.governor.dev.localhost' -o 'xorkevin.test@lists.governor.dev.localhost'
+cat ./devtools/mailinglist/testmail.eml \
+  | mailcat fmt \
+  | mailcat send \
+      -s localhost:2525 \
+      -i 'return-path@mail.governor.dev.localhost' \
+      -o 'xorkevin.test@lists.governor.dev.localhost' \
+      --dkim-selector tests \
+      --dkim-keyfile ./devtools/mailinglist/dkimkey.pem
