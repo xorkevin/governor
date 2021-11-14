1adde531d2c3851a24238b399d4b4b3552bf4e42
Kevin Wang <kevin@xorkevin.com>
2021-11-14 14:51:49 -0800

    Refactor mailinglist root

--- example/devtools/mailinglist/sendtest.sh
+++ example/devtools/mailinglist/sendtest.sh
@@ -1,10 +1,12 @@
 #!/bin/sh
 
-cat ./devtools/mailinglist/testmail.eml \
+ROOT=${0%/*}
+
+cat "$ROOT/testmail.eml" \
   | mailcat fmt \
   | mailcat send \
       -s localhost:2525 \
       -i 'return-path@mail.governor.dev.localhost' \
       -o 'xorkevin.test@lists.governor.dev.localhost' \
       --dkim-selector tests \
-      --dkim-keyfile ./devtools/mailinglist/dkimkey.pem
+      --dkim-keyfile "$ROOT/dkimkey.pem"
