f5aad9ac634f43a7994aa5bbdb14de27af6bd159
Kevin Wang <kevin@xorkevin.com>
2021-11-14 23:40:18 -0800

    Impl inreplyto test script

--- example/devtools/mailinglist/sendtest.sh
+++ example/devtools/mailinglist/sendtest.sh
@@ -2,8 +2,20 @@
 
 ROOT=${0%/*}
 
+inreplyto=$1
+references=$2
+
+args=
+if [ ! -z "$inreplyto" ]; then
+  if [ ! -z "$references" ]; then
+    args=(-s "In-Reply-To: <$inreplyto>" -s "References: $references")
+  else
+    args=(-s "In-Reply-To: <$inreplyto>" -s "References: <$inreplyto>")
+  fi
+fi
+
 cat "$ROOT/testmail.eml" \
-  | mailcat fmt \
+  | mailcat fmt "${args[@]}" \
   | mailcat send \
       -s localhost:2525 \
       -i 'return-path@mail.governor.dev.localhost' \
