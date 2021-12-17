becca61d9278f09d0e50165e3b5f0a76a5efa631
Kevin Wang <kevin@xorkevin.com>
2021-12-17 13:58:21 -0800

    Add devtools events publish

---
+++ example/devtools/events/publish.sh
@@ -0,0 +1,19 @@
+#!/bin/sh
+
+set -e
+
+ROOT=${0%/*}
+
+subject=$1
+payload=$2
+
+if [ -z "$subject" ]; then
+  printf "Must provide subject\n"
+  exit 1
+fi
+
+if [ -z "$payload" ]; then
+  payload="{\"timestamp\":$(date '+%s')}"
+fi
+
+curl --user "system:secret" --request POST "http://localhost:8080/api/events/publish?subject=$subject" --data-raw "$payload"
--- example/devtools/mailinglist/sendtest.sh
+++ example/devtools/mailinglist/sendtest.sh
@@ -1,5 +1,7 @@
 #!/bin/sh
 
+set -e
+
 ROOT=${0%/*}
 
 inreplyto=$1
