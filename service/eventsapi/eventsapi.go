9b43263a7e0382658e07185c6ce53f0e15ca59b7
Kevin Wang <kevin@xorkevin.com>
2022-10-27 20:26:40 -0700

    Use system token for events api

--- service/eventsapi/eventsapi.go
+++ service/eventsapi/eventsapi.go
@@ -2,13 +2,10 @@ package eventsapi
 
 import (
 	"context"
-	"time"
 
 	"xorkevin.dev/governor"
 	"xorkevin.dev/governor/service/pubsub"
-	"xorkevin.dev/governor/util/ksync"
-	"xorkevin.dev/governor/util/lifecycle"
-	"xorkevin.dev/kerrors"
+	"xorkevin.dev/governor/service/user/gate"
 	"xorkevin.dev/klog"
 )
 
@@ -27,14 +24,10 @@ type (
 	}
 
 	Service struct {
-		lc         *lifecycle.Lifecycle[secretAPI]
-		pubsub     pubsub.Pubsub
-		config     governor.SecretReader
-		log        *klog.LevelLogger
-		hbfailed   int
-		hbmaxfail  int
-		apirefresh time.Duration
-		wg         *ksync.WaitGroup
+		pubsub  pubsub.Pubsub
+		gate    gate.Gate
+		log     *klog.LevelLogger
+		scopens string
 	}
 
 	router struct {
@@ -62,15 +55,15 @@ func setCtxEventsAPI(inj governor.Injector, ea EventsAPI) {
 func NewCtx(inj governor.Injector) *Service {
 	return New(
 		pubsub.GetCtxPubsub(inj),
+		gate.GetCtxGate(inj),
 	)
 }
 
 // New creates a new events service
-func New(ps pubsub.Pubsub) *Service {
+func New(ps pubsub.Pubsub, g gate.Gate) *Service {
 	return &Service{
-		pubsub:   ps,
-		hbfailed: 0,
-		wg:       ksync.NewWaitGroup(),
+		pubsub: ps,
+		gate:   g,
 	}
 }
 
@@ -82,45 +75,15 @@ func (s *Service) router() *router {
 
 func (s *Service) Register(inj governor.Injector, r governor.ConfigRegistrar) {
 	setCtxEventsAPI(inj, s)
-
-	r.SetDefault("hbinterval", "5s")
-	r.SetDefault("hbmaxfail", 3)
-	r.SetDefault("apisecret", "")
-	r.SetDefault("apirefresh", "1m")
+	s.scopens = "gov." + r.Name()
 }
 
 func (s *Service) Init(ctx context.Context, c governor.Config, r governor.ConfigReader, log klog.Logger, m governor.Router) error {
 	s.log = klog.NewLevelLogger(log)
-	s.config = r
-
-	hbinterval, err := r.GetDuration("hbinterval")
-	if err != nil {
-		return kerrors.WithMsg(err, "Failed to parse hbinterval")
-	}
-	s.hbmaxfail = r.GetInt("hbmaxfail")
-	s.apirefresh, err = r.GetDuration("apirefresh")
-	if err != nil {
-		return kerrors.WithMsg(err, "Failed to parse apirefresh")
-	}
-
-	s.log.Info(ctx, "Loaded config", klog.Fields{
-		"eventsapi.hbinterval": hbinterval.String(),
-		"eventsapi.hbmaxfail":  s.hbmaxfail,
-		"eventsapi.apirefresh": s.apirefresh.String(),
-	})
 
 	ctx = klog.WithFields(ctx, klog.Fields{
 		"gov.service.phase": "run",
 	})
-
-	s.lc = lifecycle.New(
-		ctx,
-		s.handleGetAPISecret,
-		s.closeAPISecret,
-		s.handlePing,
-		hbinterval,
-	)
-	go s.lc.Heartbeat(ctx, s.wg)
 
 	sr := s.router()
 	sr.mountRoutes(governor.NewMethodRouter(m))
@@ -128,75 +91,11 @@ func (s *Service) Init(ctx context.Context, c governor.Config, r governor.Config
 	return nil
 }
 
-func (s *Service) handlePing(ctx context.Context, m *lifecycle.Manager[secretAPI]) {
-	_, err := m.Construct(ctx)
-	if err == nil {
-		s.hbfailed = 0
-		return
-	}
-	s.hbfailed++
-	if s.hbfailed < s.hbmaxfail {
-		s.log.WarnErr(ctx, kerrors.WithMsg(err, "Failed to refresh api secret"), nil)
-		return
-	}
-	s.log.Err(ctx, kerrors.WithMsg(err, "Failed max refresh attempts"), nil)
-	s.hbfailed = 0
-	// clear the cached secret because it may now be invalid
-	m.Stop(ctx)
-}
-
-type (
-	secretAPI struct {
-		Secret string `mapstructure:"secret"`
-	}
-)
-
-func (s *Service) handleGetAPISecret(ctx context.Context, m *lifecycle.Manager[secretAPI]) (*secretAPI, error) {
-	currentAuth := m.Load(ctx)
-	var apisecret secretAPI
-	if err := s.config.GetSecret(ctx, "apisecret", s.apirefresh, &apisecret); err != nil {
-		return nil, kerrors.WithMsg(err, "Invalid api secret")
-	}
-	if apisecret.Secret == "" {
-		return nil, kerrors.WithKind(nil, governor.ErrorInvalidConfig, "Empty api secret")
-	}
-	if currentAuth != nil && apisecret == *currentAuth {
-		return currentAuth, nil
-	}
-
-	m.Stop(ctx)
-
-	s.log.Info(ctx, "Refreshed api secret with new secret", nil)
-
-	m.Store(&apisecret)
-
-	return &apisecret, nil
-}
-
-func (s *Service) closeAPISecret(ctx context.Context, secret *secretAPI) {
-	// nothing to close
-}
-
-func (s *Service) getApiSecret(ctx context.Context) (string, error) {
-	if apisecret := s.lc.Load(ctx); apisecret != nil {
-		return apisecret.Secret, nil
-	}
-
-	apisecret, err := s.lc.Construct(ctx)
-	if err != nil {
-		return "", err
-	}
-	return apisecret.Secret, nil
-}
-
 func (s *Service) Start(ctx context.Context) error {
 	return nil
 }
 
 func (s *Service) Stop(ctx context.Context) {
-	if err := s.wg.Wait(ctx); err != nil {
-		s.log.WarnErr(ctx, kerrors.WithMsg(err, "Failed to stop"), nil)
-	}
 }
 
 func (s *Service) Setup(ctx context.Context, req governor.ReqSetup) error {
@@ -204,8 +103,5 @@ func (s *Service) Setup(ctx context.Context, req governor.ReqSetup) error {
 }
 
 func (s *Service) Health(ctx context.Context) error {
-	if s.lc.Load(ctx) == nil {
-		return kerrors.WithMsg(nil, "Eventsapi service not ready")
-	}
 	return nil
 }
--- service/eventsapi/route_eventsapi.go
+++ service/eventsapi/route_eventsapi.go
@@ -1,10 +1,10 @@
 package eventsapi
 
 import (
-	"crypto/subtle"
 	"net/http"
 
 	"xorkevin.dev/governor"
+	"xorkevin.dev/governor/service/user/gate"
 )
 
 type (
@@ -14,22 +14,7 @@ type (
 	}
 )
 
-func (m *router) publishEvent(c *governor.Context) {
-	username, password, ok := c.BasicAuth()
-	if !ok || username != "system" {
-		c.WriteError(governor.ErrWithRes(nil, http.StatusForbidden, "", "User is forbidden"))
-		return
-	}
-	apisecret, err := m.s.getApiSecret(c.Ctx())
-	if err != nil {
-		c.WriteError(governor.ErrWithRes(err, http.StatusInternalServerError, "", "Unable to authenticate caller"))
-		return
-	}
-	if subtle.ConstantTimeCompare([]byte(password), []byte(apisecret)) != 1 {
-		c.WriteError(governor.ErrWithRes(nil, http.StatusForbidden, "", "User is forbidden"))
-		return
-	}
-
+func (s *router) publishEvent(c *governor.Context) {
 	req := reqPublishEvent{
 		Subject: c.Query("subject"),
 	}
@@ -43,13 +28,14 @@ func (m *router) publishEvent(c *governor.Context) {
 		c.WriteError(err)
 		return
 	}
-	if err := m.s.pubsub.Publish(c.Ctx(), req.Subject, data); err != nil {
+	if err := s.s.pubsub.Publish(c.Ctx(), req.Subject, data); err != nil {
 		c.WriteError(err)
 		return
 	}
 	c.WriteStatus(http.StatusOK)
 }
 
-func (m *router) mountRoutes(r *governor.MethodRouter) {
-	r.PostCtx("/pubsub/publish", m.publishEvent)
+func (s *router) mountRoutes(r *governor.MethodRouter) {
+	scopePublish := s.s.scopens + ".publish:write"
+	r.PostCtx("/pubsub/publish", s.publishEvent, gate.System(s.s.gate, scopePublish))
 }
