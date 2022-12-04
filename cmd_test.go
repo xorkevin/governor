dbc58e88c3344a1b288a1d31d1ffef74ca3251b0
Kevin Wang <kevin@xorkevin.com>
2022-12-03 20:28:48 -0800

    Improve cmd test coverage

--- cmd_test.go
+++ cmd_test.go
@@ -4,6 +4,7 @@ import (
 	"bytes"
 	"io"
 	"os"
+	"strconv"
 	"strings"
 	"testing"
 	"testing/fstest"
@@ -23,7 +24,13 @@ type (
 
 type (
 	testClientD struct {
-		term *Terminal
+		term  *Terminal
+		flags struct {
+			key       string
+			unlock    bool
+			count     int
+			countdown []string
+		}
 	}
 )
 
@@ -32,7 +39,40 @@ func (c *testClientD) Register(inj Injector, r ConfigRegistrar, cr CmdRegistrar)
 		Usage: "echo",
 		Short: "echo input",
 		Long:  "test route that echos input",
-		Flags: nil,
+		Flags: []CmdFlag{
+			{
+				Long:     "key",
+				Short:    "i",
+				Usage:    "checked value to confirm execution",
+				Required: true,
+				Value:    &c.flags.key,
+				Default:  "bogus",
+			},
+			{
+				Long:     "unlock",
+				Short:    "s",
+				Usage:    "needed to unlock usage of the command",
+				Required: false,
+				Value:    &c.flags.unlock,
+				Default:  false,
+			},
+			{
+				Long:     "count",
+				Short:    "c",
+				Usage:    "number of items in the countdown",
+				Required: false,
+				Value:    &c.flags.count,
+				Default:  -1,
+			},
+			{
+				Long:     "countdown",
+				Short:    "t",
+				Usage:    "countdown array",
+				Required: false,
+				Value:    &c.flags.countdown,
+				Default:  []string{},
+			},
+		},
 	}, CmdHandlerFunc(c.echo))
 }
 
@@ -42,6 +82,23 @@ func (c *testClientD) Init(r ClientConfigReader, log klog.Logger, term Term, m H
 }
 
 func (c *testClientD) echo(args []string) error {
+	if !c.flags.unlock {
+		return kerrors.WithMsg(nil, "Command not unlocked")
+	}
+	if c.flags.key != "secret" {
+		return kerrors.WithMsg(nil, "Invalid key")
+	}
+	if c.flags.count < 0 {
+		return kerrors.WithMsg(nil, "Invalid count")
+	}
+	for i, n := c.flags.count, 0; i >= 1; i, n = i-1, n+1 {
+		if n >= len(c.flags.countdown) {
+			return kerrors.WithMsg(nil, "Missing countdown")
+		}
+		if c.flags.countdown[n] != strconv.Itoa(i) {
+			return kerrors.WithMsg(nil, "Wrong countdown")
+		}
+	}
 	if _, err := io.Copy(c.term.Stderr(), c.term.Stdin()); err != nil {
 		return kerrors.WithMsg(err, "Could not copy output")
 	}
@@ -94,7 +151,21 @@ func TestCmd(t *testing.T) {
 		},
 	}, nil, client)
 
-	assert.NoError(cmd.ExecArgs([]string{"sd", "echo"}))
+	assert.NoError(cmd.ExecArgs([]string{
+		"sd",
+		"echo",
+		"--key",
+		"secret",
+		"--unlock",
+		"-c",
+		"3",
+		"-t",
+		"3",
+		"-t",
+		"2",
+		"-t",
+		"1",
+	}))
 	assert.Equal("test input content", out.String())
 	out.Reset()
 }
