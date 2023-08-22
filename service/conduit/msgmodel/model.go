f135c1010306ace124474e62157efee8cc75253d
Kevin Wang <kevin@xorkevin.com>
2023-08-21 23:34:11 -0700

    Update forge conduit msg model

--- service/conduit/msgmodel/model.go
+++ service/conduit/msgmodel/model.go
@@ -36,17 +36,17 @@ type (
 	//forge:model msg
 	//forge:model:query msg
 	Model struct {
-		Chatid string `model:"chatid,VARCHAR(31)" query:"chatid"`
-		Msgid  string `model:"msgid,VARCHAR(31), PRIMARY KEY (chatid, msgid);index,chatid,kind" query:"msgid;getgroupeq,chatid;getgroupeq,chatid,msgid|lt;getgroupeq,chatid,kind;getgroupeq,chatid,kind,msgid|lt;deleq,chatid"`
-		Userid string `model:"userid,VARCHAR(31) NOT NULL" query:"userid"`
-		Timems int64  `model:"time_ms,BIGINT NOT NULL" query:"time_ms"`
-		Kind   string `model:"kind,VARCHAR(31) NOT NULL" query:"kind"`
-		Value  string `model:"value,VARCHAR(4095) NOT NULL" query:"value"`
+		Chatid string `model:"chatid,VARCHAR(31)"`
+		Msgid  string `model:"msgid,VARCHAR(31)"`
+		Userid string `model:"userid,VARCHAR(31) NOT NULL"`
+		Timems int64  `model:"time_ms,BIGINT NOT NULL"`
+		Kind   string `model:"kind,VARCHAR(31) NOT NULL"`
+		Value  string `model:"value,VARCHAR(4095) NOT NULL"`
 	}
 
 	//forge:model:query msg
 	msgValue struct {
-		Value string `query:"value;updeq,chatid,msgid|in"`
+		Value string `model:"value"`
 	}
 
 	ctxKeyRepo struct{}
@@ -107,15 +107,15 @@ func (r *repo) GetMsgs(ctx context.Context, chatid string, kind string, msgid st
 	var m []Model
 	if kind == "" {
 		if msgid == "" {
-			m, err = r.table.GetModelEqChatidOrdMsgid(ctx, d, chatid, false, limit, 0)
+			m, err = r.table.GetModelByChat(ctx, d, chatid, limit, 0)
 		} else {
-			m, err = r.table.GetModelEqChatidLtMsgidOrdMsgid(ctx, d, chatid, msgid, false, limit, 0)
+			m, err = r.table.GetModelByChatBeforeMsg(ctx, d, chatid, msgid, limit, 0)
 		}
 	} else {
 		if msgid == "" {
-			m, err = r.table.GetModelEqChatidEqKindOrdMsgid(ctx, d, chatid, kind, false, limit, 0)
+			m, err = r.table.GetModelByChatKind(ctx, d, chatid, kind, limit, 0)
 		} else {
-			m, err = r.table.GetModelEqChatidEqKindLtMsgidOrdMsgid(ctx, d, chatid, kind, msgid, false, limit, 0)
+			m, err = r.table.GetModelByChatKindBeforeMsg(ctx, d, chatid, kind, msgid, limit, 0)
 		}
 	}
 	if err != nil {
@@ -144,7 +144,7 @@ func (r *repo) EraseMsgs(ctx context.Context, chatid string, msgids []string) er
 	if err != nil {
 		return err
 	}
-	if err := r.table.UpdmsgValueEqChatidHasMsgid(ctx, d, &msgValue{
+	if err := r.table.UpdmsgValueByChatMsgs(ctx, d, &msgValue{
 		Value: "",
 	}, chatid, msgids); err != nil {
 		return kerrors.WithMsg(err, "Failed to erase chat msgs")
@@ -157,7 +157,7 @@ func (r *repo) DeleteChatMsgs(ctx context.Context, chatid string) error {
 	if err != nil {
 		return err
 	}
-	if err := r.table.DelEqChatid(ctx, d, chatid); err != nil {
+	if err := r.table.DelByChat(ctx, d, chatid); err != nil {
 		return kerrors.WithMsg(err, "Failed to delete chat msgs")
 	}
 	return nil
