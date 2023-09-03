719f3f3f049c162db3fd7813e2ee9641c29854b7
Kevin Wang <kevin@xorkevin.com>
2023-09-02 19:42:43 -0700

    Add postgres env vars for sort

--- test.sh
+++ test.sh
@@ -4,8 +4,20 @@ set -e
 
 export GOV_TEST_POSTGRES_USERNAME=postgres
 export GOV_TEST_POSTGRES_PASSWORD=admin
+export GOV_TEST_POSTGRES_DB=postgres
+export GOV_TEST_POSTGRES_HOST=localhost
+export GOV_TEST_POSTGRES_PORT=5433
 
-container_id=$(docker container run --detach --publish 5433:5432 --env POSTGRES_USER="$GOV_TEST_POSTGRES_USERNAME" --env POSTGRES_PASSWORD="$GOV_TEST_POSTGRES_PASSWORD" postgres:alpine)
+container_id=$(docker container run --detach \
+  --publish $GOV_TEST_POSTGRES_PORT:5432 \
+  --env POSTGRES_USER="$GOV_TEST_POSTGRES_USERNAME" \
+  --env POSTGRES_PASSWORD="$GOV_TEST_POSTGRES_PASSWORD" \
+  --env POSTGRES_DB="$GOV_TEST_POSTGRES_DB" \
+  --env PGDATA="/var/lib/postgresql/data/pgdata" \
+  --env POSTGRES_HOST_AUTH_METHOD="scram-sha-256" \
+  --env POSTGRES_INITDB_ARGS="--encoding UTF8 --locale=C --auth-local=trust --auth-host=scram-sha-256" \
+  --env LANG="C" \
+  postgres:alpine)
 echo "started container $container_id"
 
 cleanup() {
@@ -17,7 +29,7 @@ cleanup() {
 
 trap cleanup EXIT
 
-dsn=postgresql://$GOV_TEST_POSTGRES_USERNAME:$GOV_TEST_POSTGRES_PASSWORD@localhost:5433
+dsn=postgresql://$GOV_TEST_POSTGRES_USERNAME:$GOV_TEST_POSTGRES_PASSWORD@$GOV_TEST_POSTGRES_HOST:$GOV_TEST_POSTGRES_PORT/$GOV_TEST_POSTGRES_DB
 
 until psql $dsn -A -t -c "select 'ok';" > /dev/null 2>&1; do
   echo "waiting for container $container_id"
